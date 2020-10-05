ac4b42b0857012768fa403c3682e6d7eb1f16e5d
Gaston Noya <nvfede@gmail.com>
2020-10-05 15:44:16 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from "react";
+import "../assets/styles/app.scss";
+
+const Header = () => (
+  <Fragment>
+    <header className="header">
+      <div className="header__logo">Logo</div>
+      <nav>Navigation</nav>
+    </header>
+  </Fragment>
+);
+export default Header;
---
+++ src/containers/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+import Header from "../components/Header";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>Hello Matunga</h1>
+      </div>
+    );
+  }
+}
